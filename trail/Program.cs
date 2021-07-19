5a4e91b3219cc1b549fb97b35594b989749ef495
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-19 11:12:17 +0530

    just trail

---
+++ trail/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace trail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            Console.WriteLine("please enter the Number1");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+            int Product;
+            Product = Num1 * Num2;
+            Console.WriteLine("Product of two numbers:" + Product.ToString());
+        }
+    }
+}
